a9866197f884fcf896c089397e7e329f94dd9207
Daniel Filip <filipdaniel2000@yahoo.com>
2024-03-21 17:58:58 +0200

    Med_App#2

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <gtk/gtk.h>
 
+// TODO: ACESTE DATE TREBUIE MUTATE INTR-UN FISIER SEPARAT SI ENCAPSULATE!!!!!!!1!
+GtkWidget *name_entry;
+GtkWidget *data_entry;
+GtkWidget *description_entry;
+
 // Structura pentru stocarea datelor despre medici
 typedef struct {
     gchar *nume;
@@ -14,7 +19,88 @@ typedef struct {
     GtkWidget *loc_de_munca_entry;
     GtkWidget *lucreaza_cu_casa_de_asigurari_checkbox;
     GtkListStore *doctor_store;
+    GtkWidget *display_label;
 } AppWidgets;
+
+static void on_save_button_clicked(GtkWidget *widget, gpointer user_data) {
+    // Colectam datele introduse de utilizator
+    AppWidgets *widgets = (AppWidgets *)user_data;
+    const gchar *name = gtk_entry_get_text(GTK_ENTRY(name_entry));
+    const gchar *data = gtk_entry_get_text(GTK_ENTRY(data_entry));
+    const gchar *description = gtk_entry_get_text(GTK_ENTRY(description_entry));
+
+    gchar *display_text = g_strdup_printf("Nume: %s\nData: %s\nDescriere: %s\n", name, data, description);
+    gtk_label_set_text(GTK_LABEL(widgets->display_label), display_text);
+    g_free(display_text);
+
+    // TODO: Salvarea datelor intr-o baza de date pentru a putea fi accesate ulterior
+}
+
+
+// Functie apelata atunci cand se schimba selectia in lista de medici
+static void on_selection_changed(GtkTreeSelection *selection, gpointer user_data) {
+    GtkTreeModel *model;
+    GtkTreeIter iter;
+    AppWidgets *widgets = (AppWidgets *)user_data;
+    if (gtk_tree_selection_get_selected(selection, &model, &iter)) {
+        gchar *nume;
+        gchar *specialitate;
+        gchar *loc_de_munca;
+        gboolean lucreaza_cu_casa_de_asigurari;
+
+        gtk_tree_model_get(model, &iter, 0, &nume, 1, &specialitate, 2, &loc_de_munca, 3, &lucreaza_cu_casa_de_asigurari, -1);
+
+
+
+        // Creaza o fereastra noua pentru a introduce datele suplimentare
+        GtkWidget *details_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+        gtk_window_set_title(GTK_WINDOW(details_window), "Detalii Medic");
+        gtk_window_set_default_size(GTK_WINDOW(details_window), 300, 200);
+
+        GtkWidget *details_grid = gtk_grid_new();
+        gtk_container_add(GTK_CONTAINER(details_window), details_grid);
+
+        // Creaza label-uri si entry-uri pentru introducerea datelor suplimentare
+        GtkWidget *name_label = gtk_label_new("Nume:");
+        name_entry = gtk_entry_new();  // Make the entry widget global
+        gtk_entry_set_text(GTK_ENTRY(name_entry), nume);
+
+        GtkWidget *data_label = gtk_label_new("Data:");
+        data_entry = gtk_entry_new();  // Make the entry widget global
+
+        GtkWidget *description_label = gtk_label_new("Descriere:");
+        description_entry = gtk_entry_new();  // Make the entry widget global
+
+
+
+        // Adauga label-urile si entry-urile in grid
+        gtk_grid_attach(GTK_GRID(details_grid), name_label, 0, 0, 1, 1);
+        gtk_grid_attach(GTK_GRID(details_grid), name_entry, 1, 0, 1, 1);
+        gtk_grid_attach(GTK_GRID(details_grid), data_label, 0, 1, 1, 1);
+        gtk_grid_attach(GTK_GRID(details_grid), data_entry, 1, 1, 1, 1);
+        gtk_grid_attach(GTK_GRID(details_grid), description_label, 0, 2, 1, 1);
+        gtk_grid_attach(GTK_GRID(details_grid), description_entry, 1, 2, 1, 1);
+
+        // Creaza butonul de salvare
+        GtkWidget *save_button = gtk_button_new_with_label("Salvează");
+        gtk_grid_attach(GTK_GRID(details_grid), save_button, 0, 3, 2, 1);
+
+        widgets->display_label = gtk_label_new("");
+        gtk_grid_attach(GTK_GRID(details_grid), widgets->display_label, 0, 4, 2, 1);
+
+        // Conecteaza semnalul de click la functia de salvare
+        g_signal_connect(save_button, "clicked", G_CALLBACK(on_save_button_clicked), widgets);
+
+        // Arata toate elementele din fereastra
+        gtk_widget_show_all(details_window);
+
+        // Elibereaza memoria
+        g_free(nume);
+        g_free(specialitate);
+        g_free(loc_de_munca);
+    }
+}
+
 
 static void adauga_medic(GtkWidget *widget, AppWidgets *widgets) {
     const gchar *nume = gtk_entry_get_text(GTK_ENTRY(widgets->nume_entry));
@@ -44,6 +130,8 @@ static void activate(GtkApplication *app, gpointer user_data) {
     gtk_window_set_title(GTK_WINDOW(window), "Aplicație de programare medicală");
     gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
 
+
+
     grid = gtk_grid_new();
     gtk_container_add(GTK_CONTAINER(window), grid);
 
@@ -56,6 +144,8 @@ static void activate(GtkApplication *app, gpointer user_data) {
     GtkCellRenderer *renderer;
     GtkTreeViewColumn *column;
 
+    GtkTreeSelection *selection = gtk_tree_view_get_selection(GTK_TREE_VIEW(doctor_view));
+    g_signal_connect(selection, "changed", G_CALLBACK(on_selection_changed), widgets);
     renderer = gtk_cell_renderer_text_new();
     column = gtk_tree_view_column_new_with_attributes("Medic", renderer, "text", 0, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(doctor_view), column);
@@ -72,7 +162,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
     column = gtk_tree_view_column_new_with_attributes("Lucreaza cu Casa de Asigurari", renderer, "text", 3, NULL);
     gtk_tree_view_append_column(GTK_TREE_VIEW(doctor_view), column);
 
-    // Entry fields and labels for adding a doctor manually
+    // Adauga medic
     GtkWidget *nume_label = gtk_label_new("Nume:");
     gtk_grid_attach(GTK_GRID(grid), nume_label, 3, 1, 1, 1);
     widgets->nume_entry = gtk_entry_new();
@@ -82,6 +172,8 @@ static void activate(GtkApplication *app, gpointer user_data) {
     gtk_grid_attach(GTK_GRID(grid), specialitate_label, 3, 2, 1, 1);
     widgets->specialitate_entry = gtk_entry_new();
     gtk_grid_attach(GTK_GRID(grid), widgets->specialitate_entry, 4, 2, 1, 1);
+
+
 
     GtkWidget *loc_de_munca_label = gtk_label_new("Loc de Munca:");
     gtk_grid_attach(GTK_GRID(grid), loc_de_munca_label, 3, 3, 1, 1);
